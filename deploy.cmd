79898a030f0ef801b1e31f728909ef3f6bc49db8
Roy Leban <roy@Roys-MacBook-Pro.local>
2011-04-30 12:01:41 -0700

    added deploy

---
+++ deploy.cmd
@@ -0,0 +1,7 @@
+#!/bin/bash
+#if ["$1" == ""]; then
+#	echo "Usage: bash t/deploy1.cmd NNN  (NNN=version number)"
+#else
+#	appcfg.py update -V r$1 madswvideo/
+	appcfg.py update -V 1 madswvideo/
+#fi
