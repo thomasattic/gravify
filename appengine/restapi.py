673c095e778b09aba0d551d0eadaaeca3b7bb1a7
Roy Leban <roy@Roys-MacBook-Pro.local>
2011-04-30 21:32:52 -0700

    no 500 errors

--- appengine/restapi.py
+++ appengine/restapi.py
@@ -6,25 +6,29 @@ from django.http import HttpResponse
 def handler(req, **params):
     token = params["token"]
     
-    if (req.method == "GET"):
-        obj = RestItem.get_from_token(token)
-        
-        if obj:
-            response = HttpResponse(obj.data)
-        else:
-            response = HttpResponse("{}")
-        return response
-    
-    if (req.method == "PUT" or req.method == "POST"):
-        try:
-            body = req.raw_post_data
-        except Exception, ex:
-            body = '{"error": true}'
-        logging.info("body=" + str(body))
+    try:
+        if (req.method == "GET"):
+            obj = RestItem.get_from_token(token)
             
-        RestItem.add_data(token=token, data=body)
+            if obj:
+                response = HttpResponse(obj.data)
+            else:
+                response = HttpResponse("{}")
+            return response
         
-        response = HttpResponse()
-        return response
+        if (req.method == "PUT" or req.method == "POST"):
+            try:
+                body = req.raw_post_data
+            except Exception, ex:
+                body = '{"error": true}'
+            logging.info("body=" + str(body))
+                
+            RestItem.add_data(token=token, data=body)
+            
+            response = HttpResponse()
+            return response
+    
+    except Exception, ex:
+        return HttpResponse('{"exception": "%s"}' % (str(ex)))
     
     return HttpResponse()
