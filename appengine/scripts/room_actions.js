c25562fd99e0de5d3991ddf93ff2ad6c0ab236dc
Scott Porad <scottporad@scott-porads-macbook-pro.local>
2011-05-01 15:41:30 -0700

    adding room actions file

---
+++ appengine/scripts/room_actions.js
@@ -0,0 +1,43 @@
+/*
+					connectLink		disconnectLink	publishLink		unpublishLink	
+on load				enabled			hidden			disabled 		hidden	
+entered room		hidden			shown/enabled	enabled			hidden	
+leave room			shown/enabled	hidden			shown/disabled	hidden	
+activate camera		hidden			shown/enabled	hidden			shown/enabled	
+disable camera		hidden			shown/enabled	shown/enabled	hidden	
+*/
+
+function room_loads() {
+	show('connectLink');
+	hide('disconnectLink');
+	show_and_disable('publishLink');
+	hide('unpublishLink');
+}
+
+function room_entered() {
+	hide('connectLink');
+	show_and_enable('disconnectLink');
+	enable('publishLink');
+	hide('unpublishLink');
+}
+
+function room_left() {
+	show_and_enable('connectLink');
+	hide('disconnectLink');
+	show_and_disable('publishLink');
+	hide('unpublishLink');
+}
+
+function room_camera_on() {
+	hide('connectLink');
+	show_and_enable('disconnectLink');
+	hide('publishLink');
+	show_and_enable('unpublishLink');
+}
+
+function room_camera_off() {
+	hide('connectLink');
+	show_and_enable('disconnectLink');
+	show_and_enable('publishLink');
+	hide('unpublishLink');
+}
