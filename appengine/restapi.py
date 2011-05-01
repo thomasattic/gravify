c666193ed09a6c3500d1f9a2396d160743e99c54
Roy Leban <roy@royleban.com>
2011-05-01 13:34:31 -0700

    get title & description

--- appengine/restapi.py
+++ appengine/restapi.py
@@ -1,15 +1,19 @@
 import settings
+import logging
 
 from restitem import *
 from django.http import HttpResponse
+from google.appengine.api import urlfetch
+
+import simplejson
 
 def handler(req, **params):
     token = params["token"]
     
     try:
-        if (req.method == "GET"):
-            obj = RestItem.get_from_token(token)
-            
+        obj = RestItem.get_from_token(token)
+
+        if (req.method == "GET"):    
             if obj:
                 response = HttpResponse(obj.data)
             else:
@@ -19,11 +23,37 @@ def handler(req, **params):
         if (req.method == "PUT" or req.method == "POST"):
             try:
                 body = req.raw_post_data
+                
+                if obj and body == obj.data:
+                    data = body
+                
+                else:
+                    json = simplejson.loads(body)
+                    
+                    for item in json["items"]:
+                        logging.info(str(item))
+                        
+                        # title for new items is "newly added...", so check for description instead
+                        if not item.get("description"):
+                            fr = urlfetch.fetch("http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=json" % item.get("id"))
+                            if fr.status_code == 200:
+                                vdata = simplejson.loads(fr.content)
+                                title = vdata["entry"]["title"]["$t"]
+                                description = vdata["entry"]["media$group"]["media$description"]["$t"]
+                                
+                                item["title"] = title
+                                item["description"] = description
+                        
+                        # Weed out duplicates
+                        # Currently collisions => last ones win
+                    
+                    data = simplejson.JSONEncoder().encode(json)
+                    
             except Exception, ex:
-                body = '{"error": true}'
+                data = '{"error": true}'
             logging.info("body=" + str(body))
                 
-            RestItem.add_data(token=token, data=body)
+            RestItem.add_data(token=token, data=data)
             
             response = HttpResponse()
             return response
