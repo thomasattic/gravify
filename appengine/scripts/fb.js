57a14ffa9b9c6e6d96a77ae9925b0e36751b47d1
BeeDesk <tyip@beedesk.com>
2011-05-30 22:53:00 -0700

    Moved code into fb.js.

---
+++ appengine/scripts/fb.js
@@ -0,0 +1,43 @@
+(function() {
+  var jqt = $.jQTouch({
+    updatehash: false,
+    hashquery: true,
+    clearInitHash: false
+  });
+
+  // Items handler listens to CRUD events from model
+  var binder = {
+    added: function(event) {
+      var $newitem, $container;
+        
+      $newitem = $("#eventitem-template").tmpl(event.item);
+      $container = $("#main ul#tab-browse");
+      $container.append($newitem);
+    },
+    removed: function(event) {
+    },
+    updated: function(event) {
+    }
+  };
+
+  $(document).ready(function() {
+    var $container = $("#main"); 
+    $container.find("input[type='radio'][name='categories']").bind("click", function(event) {
+      var $target = $(this);
+      var val = $target.val();
+      if (val === "Create") {
+        if (!$container.find("#tab-create").hasClass("on")) {
+          $container.find(".tab").removeClass("on");
+          $container.find("#tab-create").addClass("on");
+        }
+      } else if (val === "Browse") {
+        if (!$container.find("#tab-browse").hasClass("on")) {
+          $container.find(".tab").removeClass("on");
+          $container.find("#tab-browse").addClass("on");
+        }
+      } else {
+        console.error("Unexpected radio button value: " + val);
+      }
+    });
+  });
+})();
